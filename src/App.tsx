3f3bfdb889a1e1ae28e309713fe426feace28f99
pcb <pcb@unit.no>
2020-09-23 16:10:10 +0200

    Added translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React, { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const App: FC = () => {
+  const { t } = useTranslation();
 
   return (
     <div>
       <h1>DLR</h1>
+      <p>{t('test-string')}</p>
     </div>
   );
 };
