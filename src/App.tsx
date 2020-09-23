3cf392adbf988dc8cd3cf32bfc628a33ba112720
pcb <pcb@unit.no>
2020-09-23 13:19:54 +0200

    husky-test

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ const App: FC = () => {
 
     var per = "fksldf";
     var nk = "fmkdlsfjlksd"
+    var nk2 = "fmkdlsfjlksd"
+    var nk3 = "fmkdlsfjlksd"
     const gg = 'øæå';
     gg = "klfdsø";
 
