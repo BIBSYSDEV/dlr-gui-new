36de2fcbf73ca12e5aeeced97b0f6f19833adc72
pcb <pcb@unit.no>
2020-09-23 13:13:02 +0200

    testing husky prettier-hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {FC} from 'react';
 
-function App() {
-  return (
-    <div >
-      <h1>DLR</h1>
-    </div>
-  );
+
+const App: FC = () => {
+
+    var per = "fksldf";
+    const gg = 'øæå';
+    gg = "klfdsø";
+
+    return (
+        <div>
+            <h1>DLR</h1>
+        </div>
+    );
 }
 
 export default App;
