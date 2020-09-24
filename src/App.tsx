22609f9fec2954409875aa586a71a5c678ff7ec3
pcb <pcb@unit.no>
2020-09-24 11:49:39 +0200

    SMILE-198 added theme and styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+  width: 100%;
+  max-width: ${({ theme }) => theme.breakpoints.values.lg + 'px'};
+  align-items: center;
+  flex-grow: 1;
+  color: ${({ theme }) => theme.palette.primary.main};
+`;
 
 const App: FC = () => {
   const { t } = useTranslation();
 
   return (
-    <div>
-      <h1>DLR</h1>
-      <p>{t('test-string')}</p>
-    </div>
+    <StyledApp>
+      <StyledContent>
+        <h1>DLR</h1>
+        <p>{t('test-string')}</p>
+      </StyledContent>
+    </StyledApp>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import i18n from './translations/i18n';
 import { I18nextProvider } from 'react-i18next';
+import { ThemeProvider } from 'styled-components';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { StylesProvider, ThemeProvider as MUIThemeProvider } from '@material-ui/styles';
+import mainTheme from './themes/mainTheme';
 
 ReactDOM.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
-      <App />
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={mainTheme}>
+          <MUIThemeProvider theme={mainTheme}>
+            <CssBaseline />
+            <App />
+          </MUIThemeProvider>
+        </ThemeProvider>
+      </StylesProvider>
     </I18nextProvider>
   </React.StrictMode>,
   document.getElementById('root')
