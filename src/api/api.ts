af1873befd4ebd8874a20f52d0dd0564ecda6be7
pcb <pcb@unit.no>
2020-09-28 12:41:36 +0200

    SMILE-201-use anononymous token to search

---
+++ src/api/api.ts
@@ -0,0 +1,65 @@
+import Axios, { AxiosRequestConfig } from 'axios';
+import constants, { StatusCode } from '../utils/constants';
+import { setAxiosDefaults } from '../utils/axios-config';
+
+// Set axios defaults only once through the app's lifetime
+setAxiosDefaults();
+
+// A completed request should return error:true|false alongside potential data
+interface CompletedApiResponse<T> {
+  error: boolean;
+  data?: T;
+}
+
+// A cancelled request should return null
+type ApiResponse<T> = CompletedApiResponse<T> | null;
+
+export const apiRequest = async <T>(axiosRequestConfig: AxiosRequestConfig): Promise<ApiResponse<T>> => {
+  try {
+    const response = await Axios(axiosRequestConfig);
+
+    if (response.status === StatusCode.OK) {
+      return { error: false, data: response.data };
+    } else if (response.status >= 400) {
+      return { error: true };
+    } else {
+      return { error: false };
+    }
+  } catch (error) {
+    return Axios.isCancel(error) ? null : { error: true };
+  }
+};
+
+export const authenticatedApiRequest = async <T>(axiosRequestConfig: AxiosRequestConfig): Promise<ApiResponse<T>> => {
+  try {
+    const idToken = ''; //await getIdToken();
+    axiosRequestConfig.headers = {
+      ...axiosRequestConfig.headers,
+      Authorization: `Bearer ${idToken}`,
+    };
+  } catch {
+    return {
+      error: true,
+    };
+  }
+  return await apiRequest(axiosRequestConfig);
+};
+
+export const getAnonymousWebToken = async () => {
+  return await apiRequest({
+    url: `${constants.guiBackendLoginPath}/anonymous`,
+    method: 'GET',
+  });
+};
+
+export const searchResources = async (query: string, token: string) => {
+  return await apiRequest({
+    url: `${constants.guiBackendResourcesSearchPath}/resources/search?query=${query}`,
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export default apiRequest;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,9 @@
-import React, { FC } from 'react';
+import React, { ChangeEvent, FC, useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { getAnonymousWebToken, searchResources } from '../api/api';
+import { toast } from 'react-toastify';
+import { Button, List, ListItem, ListItemIcon, ListItemText, TextField, Typography } from '@material-ui/core';
+import ImageIcon from '@material-ui/icons/Image';
 
 const StyledDashboard = styled.div`
   display: grid;
@@ -9,12 +12,96 @@ const StyledDashboard = styled.div`
   row-gap: 1rem;
   justify-items: center;
 `;
+const SearchFieldWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+  justify-items: center;
+`;
 
 const Dashboard: FC = () => {
+  const [token, setToken] = useState<string>('');
+  const [resultList, setResultList] = useState<any>([]);
+  const [resources, setResources] = useState<any>([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    getAnonymousWebToken().then((response) => {
+      if (response) {
+        if (response.error) {
+          toast.error('API ERROR');
+        } else {
+          window.localStorage.setItem('token', response.data as string);
+          console.log('token set in local storage');
+          setToken(response.data as string);
+        }
+      }
+    });
+  }, []);
+
+  const triggerSearch = () => {
+    searchResources(searchTerm, token).then((response) => {
+      if (response) {
+        if (response.error) {
+          toast.error('API ERROR');
+        } else {
+          let _resources: any[] = [];
+          setResultList(response);
+          // @ts-ignore
+          response.data.resourcesAsJson.map((resource: any) => _resources.push(JSON.parse(resource)));
+          setResources(_resources);
+        }
+      }
+    });
+  };
+
+  const updateSearchTermValue = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <StyledDashboard>
-      <h1>DLR</h1>
-      <Link to="/resource/43892349059034">Lenke til ressurs-side</Link>
+      <h1>Search for resources</h1>
+
+      <SearchFieldWrapper>
+        <TextField id="standard-basic" onChange={updateSearchTermValue} value={searchTerm} />
+        <Button
+          disabled={!searchTerm && searchTerm.length < 4}
+          color="primary"
+          variant="contained"
+          onClick={triggerSearch}>
+          Search
+        </Button>
+      </SearchFieldWrapper>
+
+      <div>{resultList && resultList.data && <span>Treff: {resultList.data.numFound}</span>}</div>
+
+      <List>
+        {resources &&
+          resources.length > 0 &&
+          resources.map((resource: any, index: number) => (
+            <ListItem
+              button
+              component="a"
+              href={`/resources/${resource.identifier}`}
+              key={index}
+              alignItems="flex-start">
+              <ListItemIcon>
+                <ImageIcon />
+              </ListItemIcon>
+              <ListItemText
+                primary={`${resource.features.dlr_title} (${resource.features.dlr_content_type})`}
+                secondary={
+                  <React.Fragment>
+                    <Typography style={{ display: 'block' }} component="span" variant="body2" color="textPrimary">
+                      {resource.features.dlr_submitter_email}
+                    </Typography>
+                    {resource.features.dlr_identifier_handle}
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
+          ))}
+      </List>
     </StyledDashboard>
   );
 };
