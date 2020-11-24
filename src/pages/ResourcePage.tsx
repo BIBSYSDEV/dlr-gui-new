7c7c36ad2c9e39e264a61beed8ffc33836484095
anetteOlli <ansi@unit.no>
2020-11-24 14:50:29 +0100

    SMILE-326: presenting page gradually after loading resources

--- src/pages/ResourcePage.tsx
+++ src/pages/ResourcePage.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { RouteProps, useParams } from 'react-router-dom';
-import { Creator, emptyCreator, Resource } from '../types/resource.types';
+import { Creator, Resource } from '../types/resource.types';
 import {
   getResource,
   getResourceContents,
@@ -10,6 +10,9 @@ import {
   getResourceLicenses,
 } from '../api/resourceApi';
 import { CircularProgress, Typography } from '@material-ui/core';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import { API_PATHS, API_URL } from '../utils/constants';
 import PreviewComponent from '../components/PreviewComponent';
 import ResourceMetadata from '../components/ResourceMetadata';
@@ -36,79 +39,112 @@ const ResourcePage: FC<RouteProps> = (props) => {
   const { identifier } = useParams<resourcePageParamTypes>();
   const [resource, setResource] = useState<Resource>();
   const [isLoadingResource, setIsLoadingResource] = useState<boolean>(false);
-  const [creator, setCreator] = useState<Creator[]>([emptyCreator]);
+  const [creators, setCreators] = useState<Creator[]>([]);
+  const [isLoadingCreator, setIsLoadingCreator] = useState<boolean>(false);
   const [preview, setPreview] = useState<Preview>({ type: '', theSource: '' });
+  const [isLoadingPreview, setIsLoadingPreview] = useState<boolean>(false);
   const [tags, setTags] = useState<string[]>([]);
+  const [isLoadingTags, setIsLoadingTags] = useState<boolean>(false);
   const [licenses, setLicenses] = useState<License[]>([]);
+  const [isLoadingLicenses, setIsLoadingLicenses] = useState<boolean>(false);
 
   useEffect(() => {
     const collectResourceData = async (identifier: string) => {
-      getResource(identifier).then((response) => {
-        setResource(response.data);
-      });
-      getResourceCreators(identifier).then((response) => {
-        setCreator(response.data);
-      });
-      getResourceContents(identifier).then((response) => {
-        const type = response?.data[0]?.features?.dlr_content_content_type
-          ? response?.data[0]?.features?.dlr_content_content_type
-          : '';
-        setPreview({
-          type,
-          theSource: `${API_URL}${API_PATHS.guiBackendResourcesContentPath}/${response?.data[0]?.identifier}/delivery?jwt=${localStorage.token}`,
+      getResource(identifier)
+        .then((response) => {
+          setResource(response.data);
+        })
+        .finally(() => {
+          setIsLoadingResource(false);
         });
-      });
-      getResourceTags(identifier).then((response) => {
-        setTags(response.data);
-      });
-      getResourceLicenses(identifier).then((response) => {
-        setLicenses(response.data);
-      });
+      getResourceCreators(identifier)
+        .then((response) => {
+          setCreators(response.data);
+        })
+        .finally(() => {
+          setIsLoadingCreator(false);
+        });
+      getResourceContents(identifier)
+        .then((response) => {
+          const type = response?.data[0]?.features?.dlr_content_content_type
+            ? response?.data[0]?.features?.dlr_content_content_type
+            : '';
+          setPreview({
+            type,
+            theSource: `${API_URL}${API_PATHS.guiBackendResourcesContentPath}/${response?.data[0]?.identifier}/delivery?jwt=${localStorage.token}`,
+          });
+        })
+        .finally(() => {
+          setIsLoadingPreview(false);
+        });
+      getResourceTags(identifier)
+        .then((response) => {
+          setTags(response.data);
+        })
+        .finally(() => {
+          setIsLoadingTags(false);
+        });
+      getResourceLicenses(identifier)
+        .then((response) => {
+          setLicenses(response.data);
+        })
+        .finally(() => {
+          setIsLoadingLicenses(false);
+        });
     };
 
     if (identifier) {
       setIsLoadingResource(true);
-      collectResourceData(identifier).finally(() => {
-        setIsLoadingResource(false);
-        //todo: presenter siden gradvis ?
-      });
+      setIsLoadingLicenses(true);
+      setIsLoadingCreator(true);
+      setIsLoadingPreview(true);
+      setIsLoadingTags(true);
+      collectResourceData(identifier);
     }
   }, [identifier]);
   return (
     <StyledPageContent>
-      {isLoadingResource ? (
-        <CircularProgress />
-      ) : (
+      {(isLoadingResource || isLoadingTags || isLoadingCreator || isLoadingPreview) && <CircularProgress />}
+      {!isLoadingResource && (
         <>
-          {resource && (
-            <>
-              <Typography variant="h1">{resource?.features?.dlr_title}</Typography>
-              {preview && <PreviewComponent preview={preview} />}
-              {creator[0]?.features?.dlr_creator_name && (
-                <Typography variant="h2">
-                  {t('resource.metadata.creator')}: {creator[0].features.dlr_creator_name}
-                </Typography>
-              )}
-              {resource.features.dlr_time_published && (
-                <Typography variant="body2">
-                  {t('resource.metadata.published')}: {resource.features.dlr_time_published}{' '}
-                </Typography>
-              )}
-              {resource.features.dlr_submitter_email && (
-                <Typography variant="body2">
-                  {t('resource.metadata.owner')}: {resource.features.dlr_submitter_email}{' '}
-                </Typography>
-              )}
-              {tags && resource.features.dlr_subject_nsi_id && (
-                <ResourceMetadata type={preview.type} kategori={[resource.features.dlr_subject_nsi_id]} tags={tags} />
-              )}
-              {licenses.map((license) => {
-                return <LicenseCard key={license.identifier} license={license} />;
-              })}
-            </>
+          <Typography variant="h1">{resource?.features?.dlr_title}</Typography>
+        </>
+      )}
+      {!isLoadingPreview && <>{preview && <PreviewComponent preview={preview} />}</>}
+      {!isLoadingCreator && (
+        <List>
+          {creators.map((creator) => {
+            return (
+              <ListItem>
+                <ListItemText>
+                  {t('resource.metadata.creator')}: {creator.features.dlr_creator_name}
+                </ListItemText>
+              </ListItem>
+            );
+          })}
+        </List>
+      )}
+      {!isLoadingResource && (
+        <>
+          {resource?.features.dlr_time_published && (
+            <Typography variant="body2">
+              {t('resource.metadata.published')}: {resource.features.dlr_time_published}{' '}
+            </Typography>
+          )}
+          {resource?.features.dlr_submitter_email && (
+            <Typography variant="body2">
+              {t('resource.metadata.owner')}: {resource.features.dlr_submitter_email}{' '}
+            </Typography>
           )}
         </>
       )}
+      {!isLoadingTags && tags && resource?.features.dlr_subject_nsi_id && (
+        <ResourceMetadata type={preview.type} kategori={[resource.features.dlr_subject_nsi_id]} tags={tags} />
+      )}
+      {!isLoadingLicenses &&
+        licenses.map((license) => {
+          return <LicenseCard key={license.identifier} license={license} />;
+        })}
     </StyledPageContent>
   );
 };
