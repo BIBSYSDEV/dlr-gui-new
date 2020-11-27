19499619ef1075fce2511cf5f63257fd8195266d
anetteOlli <ansi@unit.no>
2020-11-27 11:13:43 +0100

    SMILE 326: styling for License.tsx

--- src/components/License.tsx
+++ src/components/License.tsx
@@ -12,7 +12,7 @@ interface LicenseProps {
   license: License;
 }
 
-const CardWrapper = styled.div`
+const StyledCard = styled(Card)`
   padding-top: 1rem;
 `;
 
@@ -20,34 +20,34 @@ const LicenseCard: FC<LicenseProps> = (props) => {
   const { t } = useTranslation();
   const language = i18next.language;
   return (
-    <CardWrapper>
-      <Card>
-        <Typography variant="h6"> {t('license.license_information')}</Typography>
-        <div>
-          <span>
-            <Typography variant="body2">{t('license.published_with')}</Typography>
-          </span>
-        </div>
-        {language.includes('NO') && (
-          <>
+    <StyledCard>
+      <Typography variant="h6"> {t('license.license_information')}</Typography>
+      <div>
+        <span>
+          <Typography variant="body2">{t('license.published_with')}</Typography>
+        </span>
+      </div>
+      {language.includes('NO') && (
+        <>
+          <div>
             <Link to={props.license.features?.dlr_license_url_no ? props.license.features.dlr_license_url_no : ''}>
               <img src={props.license.features?.dlr_license_url_image} alt={props.license.features?.dlr_license_code} />
             </Link>
-            <br />
-            <Typography variant="caption">{props.license.features?.dlr_license_description_no}</Typography>
-          </>
-        )}
-        {!language.includes('NO') && (
-          <>
+          </div>
+          <Typography variant="caption">{props.license.features?.dlr_license_description_no}</Typography>
+        </>
+      )}
+      {!language.includes('NO') && (
+        <>
+          <div>
             <Link to={props.license.features?.dlr_license_url_en ? props.license.features.dlr_license_url_en : ''}>
               <img src={props.license.features?.dlr_license_url_image} alt={props.license.features?.dlr_license_code} />
             </Link>
-            <br />
-            <Typography variant="caption">{props.license.features?.dlr_license_description_en}</Typography>
-          </>
-        )}
-      </Card>
-    </CardWrapper>
+          </div>
+          <Typography variant="caption">{props.license.features?.dlr_license_description_en}</Typography>
+        </>
+      )}
+    </StyledCard>
   );
 };
 
