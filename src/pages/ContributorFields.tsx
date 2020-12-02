85d0f739a5f3730aa76f7381f965fd4647ebd4f4
anetteOlli <ansi@unit.no>
2020-12-02 11:06:23 +0100

    SMILE-346-register-resource-contributors, ContributorFields is now working with formik

--- src/pages/ContributorFields.tsx
+++ src/pages/ContributorFields.tsx
@@ -1,10 +1,43 @@
 import React, { FC, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { TextField } from '@material-ui/core';
-import { Contributor, Resource } from '../types/resource.types';
+import { TextField, Typography } from '@material-ui/core';
+import { Resource } from '../types/resource.types';
 import { ErrorMessage, Field, FieldArray, FieldProps, FormikProps, FormikValues } from 'formik';
 import Button from '@material-ui/core/Button';
 import { createContributor } from '../api/resourceApi';
+import Card from '../components/Card';
+import Paper from '@material-ui/core/Paper';
+import styled from 'styled-components';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
+
+const StyledPaper = styled(Paper)`
+  width: 100%;
+  padding: 3rem;
+`;
+
+const StyledField = styled(Field)`
+  display: inline-block;
+  margin-left: 2rem;
+  margin-right: 2rem;
+  padding: 1rem;
+`;
+const StyledCard = styled(Card)`
+  display: inline-block;
+  margin-top: 1rem;
+  margin-bottom: 0.3rem;
+`;
+
+const StyledTextField = styled(TextField)`
+  width: 22rem;
+  margin-left: 0.5rem;
+  margin-right: 0.5rem;
+`;
+
+const StyledButton = styled(Button)`
+  position: relative;
+  vertical-align: bottom;
+`;
 
 interface ContributorFieldsProps {
   resource: Resource;
@@ -18,38 +51,34 @@ const ContributorFields: FC<ContributorFieldsProps> = ({ resource, formikProps, 
 
   const addContributor = () => {
     createContributor(resource.identifier).then((contributorResponse) => {
-      const newContributors: Contributor[] = [];
-      if (resource.contributors) {
-        newContributors.push(...resource.contributors);
-      }
-      newContributors.push(contributorResponse.data);
-      const resourceTemp = resource;
-      resourceTemp.contributors = newContributors;
+      resource.contributors
+        ? resource.contributors.push(contributorResponse.data)
+        : (resource.contributors = [contributorResponse.data]);
       // Hacky way to force ContributorFields to update:
       setReloadState(!reloadState);
     });
   };
 
   const deleteContributor = (identifier: string) => {
-    console.log('deleting this', identifier);
+    //TODO: implement deletion
   };
 
   return (
-    <>
+    <StyledPaper>
+      <Typography variant="h1">{t('resource.metadata.contributors')}</Typography>
       <FieldArray
         name={`resource.contributors`}
         render={(arrayHelpers) => (
           <>
             {resource.contributors?.map((contributor, index) => {
               return (
-                <div key={contributor.features.dlr_contributor_identifier}>
-                  <Field name={`resource.contributors[${index}].features.dlr_contributor_name`}>
+                <StyledCard key={contributor.features.dlr_contributor_identifier}>
+                  <StyledField name={`resource.contributors[${index}].features.dlr_contributor_type`}>
                     {({ field, meta: { touched, error } }: FieldProps) => (
-                      <TextField
+                      <StyledTextField
                         {...field}
                         variant="filled"
-                        fullWidth
-                        label="contributor name"
+                        label={t('type')}
                         error={touched && !!error}
                         helperText={<ErrorMessage name={field.name} />}
                         onBlur={(event) => {
@@ -58,14 +87,13 @@ const ContributorFields: FC<ContributorFieldsProps> = ({ resource, formikProps, 
                         }}
                       />
                     )}
-                  </Field>
-                  <Field name={`resource.contributors[${index}].features.dlr_contributor_type`}>
+                  </StyledField>
+                  <StyledField name={`resource.contributors[${index}].features.dlr_contributor_name`}>
                     {({ field, meta: { touched, error } }: FieldProps) => (
-                      <TextField
+                      <StyledTextField
                         {...field}
                         variant="filled"
-                        fullWidth
-                        label="contributor type"
+                        label={t('name')}
                         error={touched && !!error}
                         helperText={<ErrorMessage name={field.name} />}
                         onBlur={(event) => {
@@ -74,41 +102,37 @@ const ContributorFields: FC<ContributorFieldsProps> = ({ resource, formikProps, 
                         }}
                       />
                     )}
-                  </Field>
+                  </StyledField>
+
                   {index > 0 && (
-                    <Button
+                    <StyledButton
+                      color="secondary"
+                      startIcon={<DeleteIcon fontSize="large" />}
+                      size="large"
                       onClick={() => {
                         deleteContributor(contributor.features.dlr_contributor_identifier);
                       }}>
-                      Delete
-                    </Button>
+                      {t('common.remove')}
+                    </StyledButton>
                   )}
-                </div>
+                </StyledCard>
               );
             })}
             <Button
               type="button"
+              variant="outlined"
+              color="primary"
+              startIcon={<AddIcon />}
               onClick={() => {
                 addContributor();
                 arrayHelpers.push({ features: { dlr_contributor_name: '', dlr_contributor_type: '' } });
               }}>
-              Legg til felt
+              {t('resource.add_contributor')}
             </Button>
           </>
         )}
       />
-
-      {resource.contributors?.map((contributor) => {
-        return (
-          <div key={contributor.features.dlr_contributor_identifier}>
-            <h3> Contributor</h3>
-            <p>Name: {contributor.features.dlr_contributor_name}</p>
-            <p>Type: {contributor.features.dlr_contributor_type}</p>
-            <p>Time created: {contributor.features.dlr_contributor_time_created}</p>
-          </div>
-        );
-      })}
-    </>
+    </StyledPaper>
   );
 };
 
