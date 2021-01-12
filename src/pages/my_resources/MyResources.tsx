85cae2c8ca55a73cdbcd8c7d6634daf3c09ff3a2
anetteOlli <ansi@unit.no>
2021-01-12 16:17:49 +0100

    SMILE-418-generate-thumbnail-for-mainfile, pixel perfect

--- src/pages/my_resources/MyResources.tsx
+++ src/pages/my_resources/MyResources.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { getMyResources } from '../../api/resourceApi';
-import { CircularProgress, List, Typography } from '@material-ui/core';
+import { CircularProgress, List, ListItem, ListItemText, Typography } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import { Resource } from '../../types/resource.types';
 import ErrorBanner from '../../components/ErrorBanner';
@@ -14,7 +14,7 @@ const StyledPageContent = styled.div`
   justify-content: space-between;
 `;
 
-const StyledPageHeader = styled(Typography)`
+const StyledListWrapper = styled.div`
   margin-top: 40px;
 `;
 
@@ -47,24 +47,32 @@ const MyResources: FC = () => {
     <StyledPageContent>
       {loadingError && <ErrorBanner />}
       <div>
-        <StyledPageHeader variant="h2">{t('resource.unpublished_resources')}</StyledPageHeader>
-        <List>
-          {isLoadingMyResources && <CircularProgress />}
-          {!isLoadingMyResources &&
-            resourcesUnpublished.length > 0 &&
-            resourcesUnpublished.map((resource: Resource, index: number) => (
-              <ResourceListItemButton key={index} resource={resource} showTimeCreated={true} />
-            ))}
-        </List>
-        <StyledPageHeader variant="h2">{t('resource.published_resources')}</StyledPageHeader>
-        <List>
-          {isLoadingMyResources && <CircularProgress />}
-          {!isLoadingMyResources &&
-            resourcesPublished.length > 0 &&
-            resourcesPublished.map((resource: Resource, index: number) => (
-              <ResourceListItemButton key={index} resource={resource} showTimeCreated={true} />
-            ))}
-        </List>
+        <StyledListWrapper>
+          <List>
+            <ListItem>
+              <Typography variant="h2">{t('resource.unpublished_resources')}</Typography>
+            </ListItem>
+            {isLoadingMyResources && <CircularProgress />}
+            {!isLoadingMyResources &&
+              resourcesUnpublished.length > 0 &&
+              resourcesUnpublished.map((resource: Resource, index: number) => (
+                <ResourceListItemButton key={index} resource={resource} showTimeCreated={true} />
+              ))}
+          </List>
+        </StyledListWrapper>
+        <StyledListWrapper>
+          <List>
+            <ListItem>
+              <Typography variant="h2">{t('resource.published_resources')}</Typography>
+            </ListItem>
+            {isLoadingMyResources && <CircularProgress />}
+            {!isLoadingMyResources &&
+              resourcesPublished.length > 0 &&
+              resourcesPublished.map((resource: Resource, index: number) => (
+                <ResourceListItemButton key={index} resource={resource} showTimeCreated={true} />
+              ))}
+          </List>
+        </StyledListWrapper>
       </div>
     </StyledPageContent>
   );
