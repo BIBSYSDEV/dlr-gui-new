b8e8ccb940cfdc3756871b063bf4f394cf58ed24
pcb <pcb@unit.no>
2021-01-13 13:26:54 +0100

    SMILE-418-styled list item-wrapper

--- src/components/ResourceListItemButton.tsx
+++ src/components/ResourceListItemButton.tsx
@@ -1,21 +1,17 @@
 import React, { FC } from 'react';
 import { Resource } from '../types/resource.types';
-import { ExtendButtonBaseTypeMap, ListItem, ListItemText, ListItemTypeMap, TypographyTypeMap } from '@material-ui/core';
+import { ListItem, ListItemText, TypographyTypeMap } from '@material-ui/core';
 import Thumbnail from './Thumbnail';
 import { useTranslation } from 'react-i18next';
 import Typography from '@material-ui/core/Typography';
 import styled from 'styled-components';
-import { OverridableComponent, OverrideProps } from '@material-ui/core/OverridableComponent';
+import { OverridableComponent } from '@material-ui/core/OverridableComponent';
 
 const StyledListItemText = styled(ListItemText)`
   padding-left: 16px;
 `;
 
-const StyledListItem: OverridableComponent<ListItemTypeMap<{ button?: false }, 'li'>> &
-  ((
-    props: { href: string } & OverrideProps<ExtendButtonBaseTypeMap<ListItemTypeMap<{ button: true }, 'div'>>, 'a'>
-  ) => JSX.Element) &
-  OverridableComponent<ExtendButtonBaseTypeMap<ListItemTypeMap<{ button: true }, 'div'>>> = styled(ListItem)`
+const StyledListItemWrapper = styled.div`
   margin-top: 16px;
 `;
 
@@ -38,34 +34,36 @@ const ResourceListItemButton: FC<ResourceListItemButtonProps> = ({
 }) => {
   const { t } = useTranslation();
   return (
-    <StyledListItem button component="a" href={`/resource/${resource.identifier}`}>
-      <Thumbnail
-        resourceIdentifier={resource.identifier}
-        alt={resource.features.dlr_title ?? t('resource.metadata.resource')}
-      />
-      <StyledListItemText
-        primary={`${resource.features.dlr_title} (${resource.features.dlr_content_type})`}
-        secondary={
-          <>
-            {showSubmitter && resource.features.dlr_submitter_email && (
-              <StyledTypography style={{ display: 'block' }} component="span" variant="body2" color="textPrimary">
-                {resource.features.dlr_submitter_email}
-              </StyledTypography>
-            )}
-            {showTimeCreated && resource.features.dlr_time_created && (
-              <StyledTypography style={{ display: 'block' }} component="span" variant="body2" color="textPrimary">
-                {resource.features.dlr_time_created}
-              </StyledTypography>
-            )}
-            {resource.features.dlr_identifier_handle && showHandle && (
-              <span>
-                {t('handle')}: {resource.features.dlr_identifier_handle}
-              </span>
-            )}
-          </>
-        }
-      />
-    </StyledListItem>
+    <StyledListItemWrapper>
+      <ListItem button component="a" href={`/resource/${resource.identifier}`}>
+        <Thumbnail
+          resourceIdentifier={resource.identifier}
+          alt={resource.features.dlr_title ?? t('resource.metadata.resource')}
+        />
+        <StyledListItemText
+          primary={`${resource.features.dlr_title} (${resource.features.dlr_content_type})`}
+          secondary={
+            <>
+              {showSubmitter && resource.features.dlr_submitter_email && (
+                <StyledTypography style={{ display: 'block' }} component="span" variant="body2" color="textPrimary">
+                  {resource.features.dlr_submitter_email}
+                </StyledTypography>
+              )}
+              {showTimeCreated && resource.features.dlr_time_created && (
+                <StyledTypography style={{ display: 'block' }} component="span" variant="body2" color="textPrimary">
+                  {resource.features.dlr_time_created}
+                </StyledTypography>
+              )}
+              {resource.features.dlr_identifier_handle && showHandle && (
+                <span>
+                  {t('handle')}: {resource.features.dlr_identifier_handle}
+                </span>
+              )}
+            </>
+          }
+        />
+      </ListItem>
+    </StyledListItemWrapper>
   );
 };
 
