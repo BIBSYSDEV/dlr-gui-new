7acb704bde41cfc0246643005a6c724bff454bdf
pcb <pcb@unit.no>
2021-01-18 14:02:45 +0100

    SMILE-476-accordion

--- src/components/AccordionRadioGroup.tsx
+++ src/components/AccordionRadioGroup.tsx
@@ -14,6 +14,10 @@ const StyledRadioBoxWrapper = styled.div`
   padding-bottom: 2rem;
 `;
 
+const StyledButton = styled(Button)`
+  padding: 0;
+`;
+
 interface AccordionRadioGroupProps {
   ariaDescription: string;
   title: string;
@@ -24,14 +28,13 @@ const AccoridionRadioGroup: FC<AccordionRadioGroupProps> = ({ ariaDescription, t
 
   return (
     <StyledRadioBoxWrapper>
-      <Button
+      <StyledButton
         aria-controls={ariaDescription}
-        size="large"
         onClick={() => setShowRadioDetails(!showRadioDetails)}
         color="primary"
         endIcon={!showRadioDetails ? <ExpandMoreIcon /> : <ExpandLessIcon />}>
         <Typography variant="h6">{title}</Typography>
-      </Button>
+      </StyledButton>
 
       {showRadioDetails && <div id={ariaDescription}>{children}</div>}
     </StyledRadioBoxWrapper>
