6b00dd36305503174e9de12e85b9ed90902efa88
anetteOlli <ansi@unit.no>
2021-01-29 08:46:43 +0100

    Code-review.

--- src/components/ErrorBanner.tsx
+++ src/components/ErrorBanner.tsx
@@ -1,9 +1,10 @@
-import React, { FC, useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { FC, useState } from 'react';
+import { TFunction, useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import { Typography } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 import { RootState } from '../state/rootReducer';
+import { User } from '../types/user.types';
 
 const StyledErrorDiv = styled.div`
   padding: 0.5rem;
@@ -16,18 +17,18 @@ interface ErrorBannerProps {
   userNeedsToBeLoggedIn?: boolean;
 }
 
+const getErrorMessage = (userNeedsToBeLoggedIn: boolean, user: User, t: TFunction<string>) => {
+  if (userNeedsToBeLoggedIn && user.id.length === 0) {
+    return t('error.403_page');
+  } else {
+    return t('error.generic');
+  }
+};
+
 const ErrorBanner: FC<ErrorBannerProps> = ({ userNeedsToBeLoggedIn = false }) => {
   const { t } = useTranslation();
   const user = useSelector((state: RootState) => state.user);
-  const [errorMessage, setErrorMessage] = useState<string>(t('error.generic'));
-
-  useEffect(() => {
-    if (userNeedsToBeLoggedIn) {
-      if (user.id.length === 0) {
-        setErrorMessage(t('error.403_page'));
-      }
-    }
-  }, [userNeedsToBeLoggedIn, user]);
+  const [errorMessage] = useState<string>(getErrorMessage(userNeedsToBeLoggedIn, user, t));
 
   return (
     <StyledErrorDiv>
