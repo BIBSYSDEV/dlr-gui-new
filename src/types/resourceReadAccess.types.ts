3d3fe2e2db11ba8b844a9d9894eba86818f508a3
anetteOlli <ansi@unit.no>
2021-02-01 15:56:16 +0100

    SMILE-534-restricted-access, added resourceReadAccess.types.ts

---
+++ src/types/resourceReadAccess.types.ts
@@ -0,0 +1,25 @@
+export interface ResourceReadAccess {
+  time: string;
+  subject: string;
+  object: string;
+  features: ResourceReadAccessFeatures;
+  profiles: ResourceReadAccessProfiles[];
+}
+
+interface ResourceReadAccessFeatures {
+  dlr_resource_app: string;
+  dlr_resource_title: string;
+}
+
+interface ResourceReadAccessProfiles {
+  time: string;
+  name: ResourceReadAccessNames;
+  expiryTime?: string;
+  ttlSeconds?: number;
+}
+
+export enum ResourceReadAccessNames {
+  Person = 'dlr_shared_resource_consumer',
+  Institution = 'dlr_shared_resource_consumer_institution',
+  Course = 'dlr_shared_resource_consumer_course',
+}
