4f6be2e2521fceaca669de2a3cc8e0313e74a361
anetteOlli <44363838+anetteOlli@users.noreply.github.com>
2021-02-18 15:52:20 +0100

    Smile 561 sitemap (#251)
    * SMILE-561-SITEMAP, page added
    
    * SMILE-561-SITEMAP, sitemap added to footer
    
    * SMILE-561-SITEMAP, shortened usage_tips
    
    * SMILE-561-SITEMAP, logout made DRY

--- src/layout/header/Logout.tsx
+++ src/layout/header/Logout.tsx
@@ -4,17 +4,17 @@ import { Button } from '@material-ui/core';
 import { API_PATHS, API_URL } from '../../utils/constants';
 import { logout } from '../../api/userApi';
 
+export function handleLogout() {
+  logout().then(() => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('tokenExpiry');
+    localStorage.removeItem('anonymousToken');
+    window.location.href = `${API_URL}${API_PATHS.guiBackendLoginPath}/dataportenLogout`;
+  });
+}
+
 const Logout: FC = () => {
   const { t } = useTranslation();
-
-  function handleLogout() {
-    logout().then(() => {
-      localStorage.removeItem('token');
-      localStorage.removeItem('tokenExpiry');
-      localStorage.removeItem('anonymousToken');
-      window.location.href = `${API_URL}${API_PATHS.guiBackendLoginPath}/dataportenLogout`;
-    });
-  }
 
   return (
     <Button color="primary" variant="contained" data-testid="logout-button" onClick={handleLogout}>
