19167269c0e93e98cedc2b4e65112c16256db730
pcb <pcb@unit.no>
2021-03-01 10:38:51 +0100

    SMILE-566-treffliste - fixed wrapping problems with licenses

--- src/components/CClogoImage.tsx
+++ src/components/CClogoImage.tsx
@@ -6,11 +6,11 @@ import ND from '../resources/images/creative_commons_logos/nd.svg';
 import Zero from '../resources/images/creative_commons_logos/zero.svg';
 import CCLogo from '../resources/images/creative_commons_logos/cc.svg';
 import BY from '../resources/images/creative_commons_logos/by.svg';
+import { Typography } from '@material-ui/core';
 
 const StyledLogoWrapper = styled.span`
   display: flex;
   align-items: flex-start;
-  margin-left: 0.3rem;
 `;
 
 const StyledImage = styled.img`
@@ -19,8 +19,17 @@ const StyledImage = styled.img`
   margin-left: 0.1rem;
 `;
 
-const StyledImageWrapper = styled.span`
+const StyledImageWrapper = styled.div`
   margin-left: 0.3rem;
+  white-space: nowrap;
+`;
+
+const StyledLicenseCode = styled(Typography)`
+  margin-left: 0.3rem;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  line-height: 1.2rem;
 `;
 
 export enum CCLogoImageRole {
@@ -35,7 +44,6 @@ interface CClogoImageProps {
 const CClogoImage: FC<CClogoImageProps> = ({ licenseCode }) => {
   return (
     <StyledLogoWrapper>
-      {licenseCode}
       <StyledImageWrapper>
         {licenseCode.toLowerCase().includes('cc') && <StyledImage src={CCLogo} alt="" />}
         {licenseCode.toLowerCase().includes('by') && <StyledImage src={BY} alt="" />}
@@ -44,6 +52,7 @@ const CClogoImage: FC<CClogoImageProps> = ({ licenseCode }) => {
         {licenseCode.toLowerCase().includes('sa') && <StyledImage src={SA} alt="" />}
         {licenseCode.toLowerCase().includes('1') && <StyledImage src={Zero} alt="" />}
       </StyledImageWrapper>
+      <StyledLicenseCode>{licenseCode}</StyledLicenseCode>
     </StyledLogoWrapper>
   );
 };
