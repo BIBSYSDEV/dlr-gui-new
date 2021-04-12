0c4d90e5ed4cf2f42e0cdb02301c641b18cb0916
anetteOlli <ansi@unit.no>
2021-04-12 16:16:59 +0200

    SMILE-632-components-for-searching-for-authorities, Select button changed

--- src/pages/edit_resource/contributors_step/AuthorityListItem.tsx
+++ src/pages/edit_resource/contributors_step/AuthorityListItem.tsx
@@ -3,11 +3,10 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Link from '@material-ui/core/Link';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import { IconButton } from '@material-ui/core';
-import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
 import { Authority } from '../../../types/authority.types';
 import { useTranslation } from 'react-i18next';
 import { BIBSYS_AUTHORITY_URL } from '../../../utils/constants';
+import Button from '@material-ui/core/Button';
 
 interface AuthorityListItemProps {
   authority: Authority;
@@ -27,13 +26,12 @@ const AuthorityListItem: FC<AuthorityListItemProps> = ({ authority, handleSelect
         }
       />
       <ListItemSecondaryAction>
-        <IconButton
+        <Button
+          variant="contained"
           data-testid={`add-verify-authority-${authority.id}`}
-          onClick={() => handleSelectedAuthorityChange(authority)}
-          edge="end"
-          aria-label={t('authority.add_authority')}>
-          <VerifiedUserIcon />
-        </IconButton>
+          onClick={() => handleSelectedAuthorityChange(authority)}>
+          {t('common.select')}
+        </Button>
       </ListItemSecondaryAction>
     </ListItem>
   );
