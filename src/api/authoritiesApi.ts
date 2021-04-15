2b25e0db0becc8337f229bb58aa5ed60fcc8c106
anetteOlli <44363838+anetteOlli@users.noreply.github.com>
2021-04-15 10:20:25 +0200

    Smile 632 components for searching for authorities (#359)
    * SMILE-632-components-for-searching-for-authorities, search for authorities api is functioning.
    
    * SMILE-632-components-for-searching-for-authorities, api-calls are working. Dialog is displaying.
    
    * SMILE-632-components-for-searching-for-authorities, refactor
    
    * SMILE-632-components-for-searching-for-authorities, translations and constants
    
    * SMILE-632-components-for-searching-for-authorities, styling
    
    * SMILE-632-components-for-searching-for-authorities, using debounce, fixed bug with initial input.
    
    * SMILE-632-components-for-searching-for-authorities, link-button
    
    * SMILE-632-components-for-searching-for-authorities, disable creator text field when authority is set.
    
    * SMILE-632-components-for-searching-for-authorities, added curator check and fixed-memory leak
    
    * SMILE-632-components-for-searching-for-authorities, functionality for contributors added.
    
    * SMILE-632-components-for-searching-for-authorities, css fix
    
    * SMILE-632-components-for-searching-for-authorities, added cypress test
    
    * SMILE-632-components-for-searching-for-authorities, fixed disabled issues
    
    * SMILE-632-components-for-searching-for-authorities, fixed abrupt closing of dialog.
    
    * SMILE-632-components-for-searching-for-authorities, added more helpertext
    
    * SMILE-632-components-for-searching-for-authorities, code-review
    
    * SMILE-632-components-for-searching-for-authorities, code-review css CreatorField.tsx
    
    * SMILE-632-components-for-searching-for-authorities, fixed-issues with margin
    
    * SMILE-632-components-for-searching-for-authorities, ContributorFields.tsx css fix
    
    * SMILE-632-components-for-searching-for-authorities, css tweaking
    
    * SMILE-632-components-for-searching-for-authorities, css tweaking
    
    * SMILE-632-components-for-searching-for-authorities, code-review
    
    * SMILE-632-components-for-searching-for-authorities, closes AuthoritySelector on change and sets creator and contributor name to authority.
    
    * SMILE-632-components-for-searching-for-authorities, fixes issue with re-fetching searchAuthorities loop
    
    * SMILE-632-components-for-searching-for-authorities, Select button changed
    
    * SMILE-632-components-for-searching-for-authorities, code-review link button
    
    * SMILE-632-components-for-searching-for-authorities, removed unused style
    
    * SMILE-632-components-for-searching-for-authorities, code-review api-constants added
    
    * SMILE-632-components-for-searching-for-authorities, moved common translation
    
    * SMILE-632-components-for-searching-for-authorities, cypress-test
    
    * SMILE-632-components-for-searching-for-authorities, code review added cypress test
    
    * SMILE-632-components-for-searching-for-authorities, code review
    
    * SMILE-632-components-for-searching-for-authorities, fetching authorities in EditResourcePage.tsx
    
    * SMILE-632-components-for-searching-for-authorities, code-review fixed spinner and pagination
    
    * SMILE-632-components-for-searching-for-authorities, fixed text not overflowing button

---
+++ src/api/authoritiesApi.ts
@@ -0,0 +1,82 @@
+import { authenticatedApiRequest } from './api';
+import { API_PATHS } from '../utils/constants';
+import {
+  Authority,
+  AuthorityResponse,
+  AuthoritySearchResponse,
+  DefaultAuthoritySearchLength,
+  DefaultAuthoritySearchOffset,
+} from '../types/authority.types';
+import { AxiosResponse } from 'axios';
+
+export const searchAuthorities = (
+  query: string,
+  offset = DefaultAuthoritySearchOffset,
+  limit = DefaultAuthoritySearchLength
+): Promise<AxiosResponse<AuthoritySearchResponse>> => {
+  return authenticatedApiRequest({
+    url: encodeURI(
+      `${API_PATHS.guiBackendAuthoritiesPath}/authorities/search?q=${query}&offset=${offset}&limit=${limit}&searchField=textsearch`
+    ),
+    method: 'GET',
+  });
+};
+
+export const getAuthoritiesForResourceCreatorOrContributor = async (
+  resourceId: string,
+  creatorOrContributorId: string
+): Promise<Authority[]> => {
+  const response: AxiosResponse<AuthorityResponse[]> = await authenticatedApiRequest({
+    url: encodeURI(
+      `${API_PATHS.guiBackendResourcesPath}/resources/${resourceId}/creators/${creatorOrContributorId}/authorities`
+    ),
+    method: 'GET',
+  });
+  return response.data.map((element: any) => {
+    return {
+      id: element.features.dlr_authority_id.replace('=', ''),
+      name: element.features.dlr_authority_name,
+      type: element.features.dlr_authority_entity_type,
+    };
+  });
+};
+
+export const postAuthorityForResourceCreatorOrContributor = (
+  resourceId: string,
+  creatorOrContributorId: string,
+  authority: Authority
+) => {
+  let data = encodeURI(
+    `dlr_authority_id==${authority.id}&dlr_authority_name=${authority.name}&dlr_authority_entity_type=${authority.type}`
+  );
+  if (authority.gender) {
+    data += `&dlr_authority_gender=${authority.gender}`;
+  }
+  return authenticatedApiRequest({
+    url: encodeURI(
+      `${API_PATHS.guiBackendResourcesPath}/resources/${resourceId}/creators/${creatorOrContributorId}/authorities`
+    ),
+    method: 'POST',
+    data,
+  });
+};
+
+export const putAuthorityForResourceCreatorOrContributor = (
+  resourceId: string,
+  creatorOrContributorId: string,
+  authority: Authority
+) => {
+  let data = encodeURI(
+    `dlr_authority_id==${authority.id}&dlr_authority_name=${authority.name}&dlr_authority_entity_type=${authority.type}`
+  );
+  if (authority.gender) {
+    data += `&dlr_authority_gender=${authority.gender}`;
+  }
+  return authenticatedApiRequest({
+    url: encodeURI(
+      `${API_PATHS.guiBackendResourcesPath}/resources/${resourceId}/creators/${creatorOrContributorId}/authorities`
+    ),
+    method: 'PUT',
+    data,
+  });
+};
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,6 @@
 export const USE_MOCK_DATA = process.env.REACT_APP_USE_MOCK === 'true';
 export const API_URL = process.env.REACT_APP_API_URL;
+export const BIBSYS_AUTHORITY_URL = 'https://authority.bibsys.no/authority/rest/authorities/html';
 
 export enum StatusCode {
   OK = 200,
