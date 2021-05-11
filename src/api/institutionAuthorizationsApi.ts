07e952b119b23292cc60ae2043ba16be83db2549
pcb <pcb@unit.no>
2021-05-11 13:20:14 +0200

    SMILE-738-admin-page-with-role-list - CodeReview- translation and type

--- src/api/institutionAuthorizationsApi.ts
+++ src/api/institutionAuthorizationsApi.ts
@@ -3,7 +3,9 @@ import { AxiosResponse } from 'axios';
 import { authenticatedApiRequest } from './api';
 import { InstitutionProfilesNames } from '../types/user.types';
 
-export const getInstitutionAuthorizations = (accessProfile: InstitutionProfilesNames): Promise<AxiosResponse<any>> => {
+export const getInstitutionAuthorizations = (
+  accessProfile: InstitutionProfilesNames
+): Promise<AxiosResponse<string[]>> => {
   return authenticatedApiRequest({
     url: encodeURI(
       `${API_PATHS.guiBackendInstitutionUserAutorizationsPath}/institutions/current/authorizations/users/accessProfiles/${accessProfile}`
