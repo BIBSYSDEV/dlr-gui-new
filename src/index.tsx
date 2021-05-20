3af85c91cd2da5d3ba790bf32286cf13d6f434fc
pcb <pcb@unit.no>
2021-05-20 13:26:46 +0200

    removed external logging

--- src/index.tsx
+++ src/index.tsx
@@ -8,28 +8,13 @@ import mainTheme from './themes/mainTheme';
 import { Provider } from 'react-redux';
 import { store } from './state/store';
 import { interceptRequestsOnMock } from './api/mock-interceptor';
-import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
 import { USE_MOCK_DATA } from './utils/constants';
 // import i18n (needs to be bundled ;))
 import './translations/i18n';
-import LogRocket from 'logrocket';
-
-LogRocket.init('8rv8br/dlr');
 
 if (USE_MOCK_DATA) {
   interceptRequestsOnMock();
 }
-
-Sentry.init({
-  dsn: 'https://83dde54f791f42f29c351d3278cd3087@o644956.ingest.sentry.io/5758486',
-  integrations: [new Integrations.BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
 
 ReactDOM.render(
   // <React.StrictMode>
