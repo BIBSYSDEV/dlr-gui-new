efae9f5c066f066a8a36ac0c7a742bd2f199bfef
anetteOlli <ansi@unit.no>
2021-05-27 15:51:39 +0200

    SMILE-716-generate-embed-code, functionality added

--- src/pages/resource/ResourceUsage.tsx
+++ src/pages/resource/ResourceUsage.tsx
@@ -14,6 +14,7 @@ const StyledTextAndButtonWrapper = styled.div`
 
 const StyledInformationWrapper = styled.div`
   display: flex;
+  align-items: flex-start;
   @media (max-width: ${({ theme }) => theme.breakpoints.values.sm + 1 + 'px'}) {
     display: block;
   }
@@ -26,7 +27,7 @@ const StyledTypography = styled(Typography)`
 `;
 
 const StyledButton = styled(Button)`
-  min-width: 10rem;
+  min-width: 12rem;
 `;
 
 const generateCitationStringPreTitle = (resource: Resource): string => {
@@ -60,6 +61,8 @@ const ResourceUsage: FC<ResourceUsageProps> = ({ resource }) => {
   const [citationTitle, setCitationTitle] = useState('');
   const [citationPostTitle, setCitationPostTitle] = useState('');
   const mountedRef = useRef(true);
+
+  const iframeText = `<iframe title="${resource.features.dlr_title}" src="${window.location.origin}/resource/${resource.identifier}/content/main?navbar=false&footer=false" width="640px" height="360px" style="border: none;" allowfullscreen="true"></iframe>`;
 
   useEffect(() => {
     const getCitation = async () => {
@@ -100,14 +103,14 @@ const ResourceUsage: FC<ResourceUsageProps> = ({ resource }) => {
     };
   }, []);
 
-  const handleCopyButtonClick = () => {
+  const handleCopyButtonClick = (text: string) => {
     try {
       //requires HTTPS connection or localhost in order to work.
-      navigator.clipboard.writeText(citationPreTitle + citationTitle + citationPostTitle);
+      navigator.clipboard.writeText(text);
     } catch (_error) {
       //Hacky workaround in case of no HTTPS connection
       const textField = document.createElement('textarea');
-      textField.innerText = citationPreTitle + citationTitle + citationPostTitle;
+      textField.innerText = text;
       document.body.appendChild(textField);
       textField.select();
       //execCommand is deprecated but copy command is still widely supported
@@ -117,19 +120,33 @@ const ResourceUsage: FC<ResourceUsageProps> = ({ resource }) => {
   };
 
   return (
-    <StyledTextAndButtonWrapper>
-      <Typography variant="caption">{t('citation.citation_link')}</Typography>
-      <StyledInformationWrapper>
-        <StyledTypography variant="body1">
-          {citationPreTitle}
-          <i>{citationTitle}</i>
-          {citationPostTitle}
-        </StyledTypography>
-        <StyledButton color="primary" variant="outlined" onClick={() => handleCopyButtonClick()}>
-          {t('citation.copy_citation').toUpperCase()}
-        </StyledButton>
-      </StyledInformationWrapper>
-    </StyledTextAndButtonWrapper>
+    <>
+      <StyledTextAndButtonWrapper>
+        <Typography variant="caption">{t('citation.citation_link')}</Typography>
+        <StyledInformationWrapper>
+          <StyledTypography variant="body1">
+            {citationPreTitle}
+            <i>{citationTitle}</i>
+            {citationPostTitle}
+          </StyledTypography>
+          <StyledButton
+            color="primary"
+            variant="outlined"
+            onClick={() => handleCopyButtonClick(citationPreTitle + citationTitle + citationPostTitle)}>
+            {t('citation.copy_citation').toUpperCase()}
+          </StyledButton>
+        </StyledInformationWrapper>
+      </StyledTextAndButtonWrapper>
+      <StyledTextAndButtonWrapper>
+        <Typography variant="caption">{t('Innbyggingskode')}</Typography>
+        <StyledInformationWrapper>
+          <StyledTypography variant="body1">{iframeText}</StyledTypography>
+          <StyledButton color="primary" variant="outlined" onClick={() => handleCopyButtonClick(iframeText)}>
+            {t('kopier innebyggingskode').toUpperCase()}
+          </StyledButton>
+        </StyledInformationWrapper>
+      </StyledTextAndButtonWrapper>
+    </>
   );
 };
 
