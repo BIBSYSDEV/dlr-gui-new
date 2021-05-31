de08087d2a5bdd035b92a6ca45388ad3af553d22
anetteOlli <ansi@unit.no>
2021-05-31 08:55:49 +0200

    SMILE-740-curator-DOI-request-list, fetching DOI-requests

--- src/pages/worklist/DOIRequestList.tsx
+++ src/pages/worklist/DOIRequestList.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { CircularProgress, Typography } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
+import { getWorkListItemDOI } from '../../api/workListApi';
+import ErrorBanner from '../../components/ErrorBanner';
 
 const DOIRequestList = () => {
   const { t } = useTranslation();
+  const [isLoading, setIsloading] = useState(false);
+  const [loadingError, setLoadingError] = useState<Error>();
+  const [worklistDoi, setWorklistDoi] = useState<any>([]);
+
+  useEffect(() => {
+    const fetchWorklisDoi = async () => {
+      try {
+        setIsloading(true);
+        setLoadingError(undefined);
+        const worklistDoiResponse = await getWorkListItemDOI();
+        setWorklistDoi(worklistDoiResponse.data);
+      } catch (error) {
+        setLoadingError(error);
+      } finally {
+        setIsloading(false);
+      }
+    };
+    fetchWorklisDoi();
+  }, []);
 
   return (
     <>
-      <Typography gutterBottom variant="h2">
-        {t('work_list.doi_request_list')}
-      </Typography>
+      {loadingError && <ErrorBanner userNeedsToBeLoggedIn={true} error={loadingError} />}
+      {isLoading ? (
+        <CircularProgress />
+      ) : (
+        <>
+          <Typography gutterBottom variant="h2">
+            {t('work_list.doi_request_list')}
+          </Typography>
+          {JSON.stringify(worklistDoi)}
+        </>
+      )}
     </>
   );
 };
