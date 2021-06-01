b87c6748f1c2f9dfcd022c34f369255d554a18ed
pcb <pcb@unit.no>
2021-06-01 11:03:18 +0200

    SMILE-749-report-button test

--- src/pages/resource/ResourceUsage.tsx
+++ src/pages/resource/ResourceUsage.tsx
@@ -6,20 +6,11 @@ import { format } from 'date-fns';
 import { TFunction, useTranslation } from 'react-i18next';
 import { getCitationFromCrossCite } from '../../api/resourceApi';
 import styled from 'styled-components';
+import { Grid } from '@material-ui/core';
 import EmbedButtons from './EmbedButtons';
 
-const StyledUsageWrapper = styled.div`
-  margin-bottom: 2rem;
-`;
-const StyledTextAndButtonWrapper = styled.div`
-  display: block;
-`;
-
-const StyledInformationWrapper = styled.div`
-  display: flex;
-  @media (max-width: ${({ theme }) => theme.breakpoints.values.sm + 1 + 'px'}) {
-    display: block;
-  }
+const StyledGridContainer = styled(Grid)`
+  margin-top: 1rem;
 `;
 
 const StyledTypography = styled(Typography)`
@@ -29,7 +20,7 @@ const StyledTypography = styled(Typography)`
 `;
 
 const StyledButton = styled(Button)`
-  min-width: 10rem;
+  min-width: 12rem;
 `;
 
 const generateCitationStringPreTitle = (resource: Resource): string => {
@@ -63,6 +54,12 @@ const ResourceUsage: FC<ResourceUsageProps> = ({ resource }) => {
   const [citationTitle, setCitationTitle] = useState('');
   const [citationPostTitle, setCitationPostTitle] = useState('');
   const mountedRef = useRef(true);
+
+  const iframeText = `<iframe title="${resource.features.dlr_title.replaceAll('"', '')}" src="${
+    window.location.origin
+  }/resource/${
+    resource.identifier
+  }/content/main?navbar=false&footer=false" width="640px" height="360px" style="border: none;" allowfullscreen="true"></iframe>`;
 
   useEffect(() => {
     const getCitation = async () => {
@@ -103,14 +100,14 @@ const ResourceUsage: FC<ResourceUsageProps> = ({ resource }) => {
     };
   }, []);
 
-  const handleCopyButtonClick = () => {
+  const handleCopyButtonClick = (text: string) => {
     try {
       //requires HTTPS connection or localhost in order to work.
-      navigator.clipboard.writeText(citationPreTitle + citationTitle + citationPostTitle);
+      navigator.clipboard.writeText(text);
     } catch (_error) {
       //Hacky workaround in case of no HTTPS connection
       const textField = document.createElement('textarea');
-      textField.innerText = citationPreTitle + citationTitle + citationPostTitle;
+      textField.innerText = text;
       document.body.appendChild(textField);
       textField.select();
       //execCommand is deprecated but copy command is still widely supported
@@ -120,25 +117,43 @@ const ResourceUsage: FC<ResourceUsageProps> = ({ resource }) => {
   };
 
   return (
-    <StyledUsageWrapper>
-      <Typography variant="h2" gutterBottom>
-        {t('common.usage')}
-      </Typography>
-      <StyledTextAndButtonWrapper>
-        <Typography variant="caption">{t('citation.citation_link')}</Typography>
-        <StyledInformationWrapper>
+    <>
+      <StyledGridContainer container spacing={3}>
+        <Grid item xs={12} sm={8}>
+          <Typography variant="caption">{t('citation.citation_link')}</Typography>
           <StyledTypography variant="body1">
             {citationPreTitle}
             <i>{citationTitle}</i>
             {citationPostTitle}
           </StyledTypography>
-          <StyledButton color="primary" variant="outlined" onClick={() => handleCopyButtonClick()}>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <StyledButton
+            color="primary"
+            variant="outlined"
+            onClick={() => handleCopyButtonClick(citationPreTitle + citationTitle + citationPostTitle)}>
             {t('citation.copy_citation').toUpperCase()}
           </StyledButton>
-        </StyledInformationWrapper>
-      </StyledTextAndButtonWrapper>
-      <EmbedButtons resource={resource} />
-    </StyledUsageWrapper>
+        </Grid>
+      </StyledGridContainer>
+      <StyledGridContainer container spacing={3}>
+        <Grid item xs={12} sm={8}>
+          <Typography variant="caption">{t('embed.embed_code')}</Typography>
+
+          <StyledTypography variant="body1">{iframeText}</StyledTypography>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <StyledButton color="primary" variant="outlined" onClick={() => handleCopyButtonClick(iframeText)}>
+            {t('embed.copy_embed_code').toUpperCase()}
+          </StyledButton>
+        </Grid>
+      </StyledGridContainer>
+      <StyledGridContainer container spacing={3}>
+        <Grid item xs={12}>
+          <EmbedButtons resource={resource} />
+        </Grid>
+      </StyledGridContainer>
+    </>
   );
 };
 
