f315c34fbd2a2da753d4889635cad00e22e60a7c
anetteOlli <44363838+anetteOlli@users.noreply.github.com>
2021-06-08 12:41:20 +0200

    Smile 794 handle deling (#531)
    * SMILE-794-handle-deling, social media sharing
    
    * SMILE-794-handle-deling, added translations.
    
    * SMILE-794-handle-deling, run prettier
    
    * SMILE-794-handle-deling, code-review

---
+++ src/components/SocialMediaSharing.tsx
@@ -0,0 +1,53 @@
+import React, { FC } from 'react';
+import { Grid } from '@material-ui/core';
+import {
+  EmailIcon,
+  EmailShareButton,
+  FacebookIcon,
+  FacebookShareButton,
+  LinkedinIcon,
+  LinkedinShareButton,
+  RedditIcon,
+  RedditShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+} from 'react-share';
+
+interface SocialMediaSharingProps {
+  resourceTitle: string;
+  url: string;
+}
+
+const SocialMediaSharing: FC<SocialMediaSharingProps> = ({ resourceTitle, url }) => {
+  return (
+    <Grid container spacing={1}>
+      <Grid item>
+        <FacebookShareButton quote={resourceTitle} url={url}>
+          <FacebookIcon size={32} round />
+        </FacebookShareButton>
+      </Grid>
+      <Grid item>
+        <TwitterShareButton url={url} title={resourceTitle}>
+          <TwitterIcon size={32} round />
+        </TwitterShareButton>
+      </Grid>
+      <Grid item>
+        <LinkedinShareButton url={url} title={resourceTitle}>
+          <LinkedinIcon size={32} round />
+        </LinkedinShareButton>
+      </Grid>
+      <Grid item>
+        <RedditShareButton url={url} title={resourceTitle}>
+          <RedditIcon size={32} round />
+        </RedditShareButton>
+      </Grid>
+      <Grid item>
+        <EmailShareButton url={url} subject={resourceTitle} body="body">
+          <EmailIcon size={32} round />
+        </EmailShareButton>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default SocialMediaSharing;
