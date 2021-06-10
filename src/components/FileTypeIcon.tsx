db205447bcd4bd021bffac04fe8eda10e44fd88d
anetteOlli <44363838+anetteOlli@users.noreply.github.com>
2021-06-10 11:27:37 +0200

    Smile 814 more typos plus legacy resource types viewing (#542)
    * SMILE-814-more-typos-plus-legacy-resource-types-viewing, changed translations
    
    * SMILE-814-more-typos-plus-legacy-resource-types-viewing, added icons and more institution casing.

--- src/components/FileTypeIcon.tsx
+++ src/components/FileTypeIcon.tsx
@@ -1,17 +1,51 @@
 import React from 'react';
-import { ResourceFeatureTypes } from '../types/resource.types';
+import { LegacyResourceFeatureTypes, ResourceFeatureTypes } from '../types/resource.types';
 import SlideshowIcon from '@material-ui/icons/Slideshow';
 import PhotoOutlinedIcon from '@material-ui/icons/PhotoOutlined';
 import VolumeUpIcon from '@material-ui/icons/VolumeUp';
 import VideocamIcon from '@material-ui/icons/Videocam';
 import DescriptionOutlinedIcon from '@material-ui/icons/DescriptionOutlined';
+import OndemandVideoIcon from '@material-ui/icons/OndemandVideo';
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import EventIcon from '@material-ui/icons/Event';
+import TouchAppIcon from '@material-ui/icons/TouchApp';
+import SquareFootIcon from '@material-ui/icons/SquareFoot';
+import CodeIcon from '@material-ui/icons/Code';
+import CallSplitIcon from '@material-ui/icons/CallSplit';
 
 export const getStyledFileTypeIcon = (type: string) => {
-  if (type.toUpperCase() === ResourceFeatureTypes.audio.toUpperCase()) return <VolumeUpIcon />;
-  if (type.toUpperCase() === ResourceFeatureTypes.image.toUpperCase()) return <PhotoOutlinedIcon />;
-  if (type.toUpperCase() === ResourceFeatureTypes.presentation.toUpperCase()) return <SlideshowIcon />;
-  if (type.toUpperCase() === ResourceFeatureTypes.simulation.toUpperCase()) return <SlideshowIcon />;
-  if (type.toUpperCase() === ResourceFeatureTypes.video.toUpperCase()) return <VideocamIcon />;
-  if (type.toUpperCase() === ResourceFeatureTypes.document.toUpperCase()) return <DescriptionOutlinedIcon />;
-  return <DescriptionOutlinedIcon />; //default
+  switch (type.toUpperCase()) {
+    case ResourceFeatureTypes.audio.toUpperCase():
+      return <VolumeUpIcon />;
+    case ResourceFeatureTypes.image.toUpperCase():
+      return <PhotoOutlinedIcon />;
+    case ResourceFeatureTypes.presentation.toUpperCase():
+      return <SlideshowIcon />;
+    case ResourceFeatureTypes.simulation.toUpperCase():
+      return <SlideshowIcon />;
+    case ResourceFeatureTypes.video.toUpperCase():
+      return <VideocamIcon />;
+    case ResourceFeatureTypes.document.toUpperCase():
+      return <DescriptionOutlinedIcon />;
+    case LegacyResourceFeatureTypes.AudioVisual.toUpperCase():
+      return <OndemandVideoIcon />;
+    case LegacyResourceFeatureTypes.Collection.toUpperCase():
+      return <LibraryBooksIcon />;
+    case LegacyResourceFeatureTypes.Event.toUpperCase():
+      return <EventIcon />;
+    case LegacyResourceFeatureTypes.InteractiveResource.toUpperCase():
+      return <TouchAppIcon />;
+    case LegacyResourceFeatureTypes.Model.toUpperCase():
+      return <SquareFootIcon />;
+    case LegacyResourceFeatureTypes.PhysicalObject.toUpperCase():
+      return <SquareFootIcon />;
+    case LegacyResourceFeatureTypes.Software.toUpperCase():
+      return <CodeIcon />;
+    case LegacyResourceFeatureTypes.Sound.toUpperCase():
+      return <VolumeUpIcon />;
+    case LegacyResourceFeatureTypes.Workflow.toUpperCase():
+      return <CallSplitIcon />;
+    default:
+      return <DescriptionOutlinedIcon />;
+  }
 };
