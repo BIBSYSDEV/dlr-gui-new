d62de70916f99f8932eeff11ec348d64d10754bf
pcb <pcb@unit.no>
2021-06-23 13:30:25 +0200

    SMILE-837-buttons - search-input

--- src/pages/dashboard/SearchInput.tsx
+++ src/pages/dashboard/SearchInput.tsx
@@ -16,9 +16,18 @@ const StyledForm = styled.form`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const StyledSearchWrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+  max-width: ${StyleWidths.width3};
   @media (max-width: ${({ theme }) => theme.breakpoints.values.sm + 'px'}) {
     flex-direction: column;
-    justify-content: flex-start;
+    align-items: flex-start;
+  }
+  & .MuiTextField-root {
+    margin-top: 3px;
   }
 `;
 
@@ -26,23 +35,25 @@ const StyledTextField = styled(TextField)`
   @media (max-width: ${({ theme }) => theme.breakpoints.values.sm + 'px'}) {
     width: 100%;
   }
+  font-size: 2rem;
   @media (min-width: ${({ theme }) => theme.breakpoints.values.sm + 'px'}) {
     width: ${StyleWidths.width3};
     margin-right: 1rem;
+  }
+  & .MuiInputBase-root {
+    height: 3rem;
   }
 `;
 
 const ButtonWrapper = styled.div`
   display: flex;
   align-items: baseline;
-  @media (max-width: ${({ theme }) => theme.breakpoints.values.sm + 'px'}) {
-    margin-top: 1rem;
-  }
 `;
 
 const StyledButton = styled(Button)`
   min-width: 7rem;
   margin-right: 0.5rem;
+  height: 3rem;
 `;
 
 interface SearchInputProps {
@@ -78,34 +89,36 @@ const SearchInput: FC<SearchInputProps> = ({ setQueryObject, queryObject }) => {
 
   return (
     <StyledForm onSubmit={handleSubmit}>
-      <StyledTextField
-        data-testid="search-for-resource-input"
-        variant="filled"
-        fullWidth
-        id="search-textfield"
-        onChange={updateSearchTermValue}
-        value={searchTerm}
-        label={t('common.search')}
-      />
-      <ButtonWrapper>
-        <StyledButton
-          data-testid="search-for-resource-submit"
-          startIcon={<SearchIcon />}
-          disabled={!searchTerm && searchTerm.length < 4}
-          color="primary"
-          variant="contained"
-          type="submit">
-          {t('common.search').toUpperCase()}
-        </StyledButton>
-        <HelperTextPopover
-          ariaButtonLabel={t('explanation_text.search_input_helper_aria_label')}
-          popoverId={'search-input-explainer'}>
-          <Typography>
-            {`${t('explanation_text.search_input_helper_text')} `}
-            <Link href={'/search-helper'}>{t('search_tricks.page_title')}</Link>
-          </Typography>
-        </HelperTextPopover>
-      </ButtonWrapper>
+      <StyledSearchWrapper>
+        <StyledTextField
+          data-testid="search-for-resource-input"
+          variant="outlined"
+          fullWidth
+          id="search-textfield"
+          onChange={updateSearchTermValue}
+          value={searchTerm}
+          label={t('common.search')}
+        />
+        <ButtonWrapper>
+          <StyledButton
+            data-testid="search-for-resource-submit"
+            startIcon={<SearchIcon />}
+            disabled={!searchTerm && searchTerm.length < 4}
+            color="primary"
+            variant="contained"
+            type="submit">
+            {t('common.search').toUpperCase()}
+          </StyledButton>
+          <HelperTextPopover
+            ariaButtonLabel={t('explanation_text.search_input_helper_aria_label')}
+            popoverId={'search-input-explainer'}>
+            <Typography>
+              {`${t('explanation_text.search_input_helper_text')} `}
+              <Link href={'/search-helper'}>{t('search_tricks.page_title')}</Link>
+            </Typography>
+          </HelperTextPopover>
+        </ButtonWrapper>
+      </StyledSearchWrapper>
     </StyledForm>
   );
 };
