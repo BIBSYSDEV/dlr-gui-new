aef075e3c05e7a07cd55c91d6a34e4b15b25f563
anetteOlli <ansi@unit.no>
2021-06-24 14:24:22 +0200

    SMILE-433-detailed-form-error-message-during-registration, processing errors

--- src/pages/edit_resource/ResourceFormErrors.tsx
+++ src/pages/edit_resource/ResourceFormErrors.tsx
@@ -5,6 +5,8 @@ import { Typography } from '@material-ui/core';
 import { StyledContentWrapper, StyledSchemaPartColored } from '../../components/styled/Wrappers';
 import { Colors } from '../../themes/mainTheme';
 import WarningIcon from '@material-ui/icons/Warning';
+import { useFormikContext } from 'formik';
+import { Resource } from '../../types/resource.types';
 
 const StyledPanel = styled.div`
   display: flex;
@@ -21,13 +23,72 @@ const StyledWarningIcon = styled(WarningIcon)`
 
 const ResourceFormErrors = () => {
   const { t } = useTranslation();
+  const { errors } = useFormikContext<Resource>();
+
   return (
     <StyledPanel data-testid="form-errors-panel">
       <StyledSchemaPartColored color={Colors.DangerLight}>
         <StyledContentWrapper>
-          <Typography variant={'h5'} component="h2">
-            <StyledWarningIcon /> {t('feedback.form_errors')}
+          <Typography gutterBottom variant={'h5'} component="h2">
+            {t('feedback.form_errors')}
           </Typography>
+          {errors.features?.dlr_title && (
+            <Typography gutterBottom>
+              <StyledWarningIcon />
+              Ressurstittel er {errors.features?.dlr_title.toLowerCase()}
+            </Typography>
+          )}
+          {errors.features?.dlr_type && (
+            <Typography gutterBottom>
+              <StyledWarningIcon />
+              Ressurstype kan kun være noen av forslagene
+            </Typography>
+          )}
+          {errors.creators && (
+            <>
+              {Array.isArray(errors.creators) && errors.creators.length > 1 ? (
+                <Typography gutterBottom>
+                  <StyledWarningIcon />
+                  Alle felt for opphavernavn må fylles ut, eller slette tomme felt.
+                </Typography>
+              ) : (
+                <Typography gutterBottom>
+                  <StyledWarningIcon />
+                  Det må minst være én opphaver utfylt.
+                </Typography>
+              )}
+            </>
+          )}
+          {errors.contributors && (
+            <Typography gutterBottom>
+              <StyledWarningIcon />
+              Alle felt for bidragsytertype og navn på bidragsyter må fylles ut, eventuelt slette unødvendige felt
+            </Typography>
+          )}
+          {errors.contents && (
+            <Typography gutterBottom>
+              <StyledWarningIcon />
+              Filtittel på hovedfil må være fylt ut.
+            </Typography>
+          )}
+          {errors.features?.dlr_licensehelper_contains_other_peoples_work && (
+            <Typography gutterBottom>
+              <StyledWarningIcon />
+              Spørsmålet "Inneholder ressursen andres arbeid?" må besvares
+            </Typography>
+          )}
+          {errors.features?.dlr_licensehelper_usage_cleared_with_owner && (
+            <Typography gutterBottom>
+              <StyledWarningIcon />
+              Spørsmålet "Er arbeidet rettighetsklarert?" må besvares
+            </Typography>
+          )}
+          {errors.licenses && (
+            <Typography gutterBottom>
+              <StyledWarningIcon />
+              Du må velge en lisens
+            </Typography>
+          )}
         </StyledContentWrapper>
       </StyledSchemaPartColored>
     </StyledPanel>
