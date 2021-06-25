2bdccc2b8702a0af8b142529f03099deb062f6f0
pcb <pcb@unit.no>
2021-06-25 13:00:05 +0200

    SMILE-852-kaltura-list - dialog and styling

--- src/pages/edit_resource/KalturaRegistration.tsx
+++ src/pages/edit_resource/KalturaRegistration.tsx
@@ -1,15 +1,34 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import StartRegistrationMethodAccordion from './StartRegistrationMethodAccordion';
 import VideocamIcon from '@material-ui/icons/Videocam';
 import { getMyKalturaPresentations } from '../../api/resourceApi';
-import { Button, CircularProgress, List, Typography } from '@material-ui/core';
-import { Colors, StyleWidths } from '../../themes/mainTheme';
+import {
+  Button,
+  CircularProgress,
+  DialogContent,
+  Grid,
+  Link,
+  List,
+  Typography,
+  useMediaQuery,
+} from '@material-ui/core';
+import { Colors, DeviceWidths, StyleWidths } from '../../themes/mainTheme';
 import ErrorBanner from '../../components/ErrorBanner';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+
+const FormDialogTitleId = 'kaltura-dialog-title';
 
 const StyledBody = styled.div`
   width: 100%;
+`;
+
+const StyledDialogActions = styled(DialogActions)`
+  padding: 1rem;
 `;
 
 const StyledList = styled(List)`
@@ -17,6 +36,17 @@ const StyledList = styled(List)`
   flex-direction: column;
   align-items: center;
   width: 100%;
+`;
+
+const StyledImageWrapper: any = styled.div`
+  min-height: 5rem;
+  height: 5rem;
+  min-width: 7.85rem;
+  width: 7.85rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid ${Colors.DescriptionPageGradientColor2};
 `;
 
 const StyledResultItem = styled.li`
@@ -53,55 +83,102 @@ interface KalturaRegistrationProps {
 
 const KalturaRegistration: FC<KalturaRegistrationProps> = ({ expanded, onChange, onSubmit }) => {
   const { t } = useTranslation();
-  const [result, setResult] = useState<KalturaPresentation[]>();
-  const [getKalturaPresentationError, setGetKalturaPresentationError] = useState<Error>();
-  const [busyGettingKalturaPresentation, setBusyGettingKalturaPresentation] = useState(false);
+  const [kalturaResources, setKalturaResources] = useState<KalturaPresentation[]>();
+  const [getKalturaResourcesError, setGetKalturaResourcesError] = useState<Error>();
+  const [busyGettingKalturaResources, setBusyGettingKalturaResources] = useState(false);
+  const fullScreenDialog = useMediaQuery(`(max-width:${DeviceWidths.md - 1}px)`);
+  const [open, setOpen] = useState(false);
 
-  useEffect(() => {
-    const getKaltura = async () => {
-      try {
-        setBusyGettingKalturaPresentation(true);
-        setGetKalturaPresentationError(undefined);
-        setResult((await getMyKalturaPresentations()).data);
-      } catch (error) {
-        setGetKalturaPresentationError(undefined);
-      } finally {
-        setBusyGettingKalturaPresentation(false);
-      }
-    };
-    expanded && getKaltura();
-  }, [expanded]);
+  const handleClickOpen = async () => {
+    setOpen(true);
+    try {
+      setBusyGettingKalturaResources(true);
+      setGetKalturaResourcesError(undefined);
+      setKalturaResources((await getMyKalturaPresentations()).data);
+    } catch (error) {
+      setGetKalturaResourcesError(undefined);
+    } finally {
+      setBusyGettingKalturaResources(false);
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleUseResource = (kalturaResourceId: string) => {
+    console.log('valgt: ' + kalturaResourceId);
+    setOpen(false);
+  };
 
   return (
-    <StartRegistrationMethodAccordion
-      headerLabel={t('Start med å velge en video fra din Kaltura-konto(translate)')}
-      icon={<VideocamIcon className="icon" />}
-      expanded={expanded}
-      onChange={onChange}
-      ariaControls="resource-method-link"
-      dataTestId="new-resource-link">
-      <StyledBody>
-        <StyledList>
-          {busyGettingKalturaPresentation ? (
-            <CircularProgress />
-          ) : (
-            result &&
-            result.map((resultItem) => (
-              <StyledResultItem>
-                <StyledImage src={resultItem.thumbnailUrl} />
-                <a href={resultItem.url}>
-                  <Typography> {resultItem.title}</Typography>
-                </a>
-                <Button variant="outlined">{t('common.use')}</Button>
-              </StyledResultItem>
-            ))
-          )}
-        </StyledList>
-        {getKalturaPresentationError && (
-          <ErrorBanner userNeedsToBeLoggedIn={true} error={getKalturaPresentationError} />
-        )}
-      </StyledBody>
-    </StartRegistrationMethodAccordion>
+    <>
+      <StartRegistrationMethodAccordion
+        headerLabel={t('Start med å velge en video fra din Kaltura-konto(translate)')}
+        icon={<VideocamIcon className="icon" />}
+        expanded={expanded}
+        onChange={onChange}
+        ariaControls="resource-method-link"
+        dataTestId="new-resource-link">
+        <StyledBody>
+          <Button
+            data-testid="open-kaltura-dialog-button"
+            variant="contained"
+            fullWidth
+            color="primary"
+            onClick={handleClickOpen}>
+            {t('VIS MINE KALTURA-RESSURSER (i18n)')}
+          </Button>
+        </StyledBody>
+      </StartRegistrationMethodAccordion>
+      <Dialog
+        maxWidth={'md'}
+        fullWidth
+        fullScreen={fullScreenDialog}
+        open={open}
+        onClose={handleClose}
+        aria-labelledby={FormDialogTitleId}>
+        <DialogTitle id={FormDialogTitleId}>{t('kaltura.my_resources')}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{t('kaltura.choose_a_resource')}. </DialogContentText>
+          <StyledList>
+            {busyGettingKalturaResources ? (
+              <CircularProgress />
+            ) : kalturaResources ? (
+              kalturaResources.map((resultItem) => (
+                <StyledResultItem key={resultItem.id}>
+                  <Grid container spacing={1}>
+                    <Grid item xs={12} sm={4}>
+                      <StyledImageWrapper>
+                        <StyledImage src={resultItem.thumbnailUrl} />
+                      </StyledImageWrapper>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <Link href={resultItem.url} target="_blank">
+                        <Typography> {resultItem.title}</Typography>
+                      </Link>
+                    </Grid>
+                    <Grid item xs={12} sm={2} alignItems="center">
+                      <Button variant="outlined" onClick={() => handleUseResource(resultItem.id)}>
+                        {t('common.use')}
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </StyledResultItem>
+              ))
+            ) : (
+              <Typography>{t('Ingen ressurser funnet')}</Typography>
+            )}
+          </StyledList>
+          {getKalturaResourcesError && <ErrorBanner userNeedsToBeLoggedIn={true} error={getKalturaResourcesError} />}
+        </DialogContent>
+        <StyledDialogActions>
+          <Button data-testid="kalture-dialog-close-button" variant="outlined" onClick={handleClose} color="primary">
+            {t('common.close')}
+          </Button>
+        </StyledDialogActions>
+      </Dialog>
+    </>
   );
 };
 
