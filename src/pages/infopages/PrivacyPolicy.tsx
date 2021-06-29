7bbf5598dc9ba005f6d4ae4dbfdd23465472ae3b
pcb <pcb@unit.no>
2021-06-29 10:41:32 +0200

    SMILE-758-privacypolicy

--- src/pages/infopages/PrivacyPolicy.tsx
+++ src/pages/infopages/PrivacyPolicy.tsx
@@ -4,7 +4,8 @@ import { useTranslation } from 'react-i18next';
 import Heading from '../../components/Heading';
 import Card from '../../components/Card';
 
-import { StyledInformationWrapper, StyledNormalTextPreWrapped } from '../../components/styled/Wrappers';
+import { StyledInformationWrapper } from '../../components/styled/Wrappers';
+import { Link, List, ListItem } from '@material-ui/core';
 
 const StyledHeading = styled(Heading)`
   text-align: center;
@@ -18,7 +19,22 @@ const PrivacyPolicy = () => {
     <StyledInformationWrapper data-testid="privacy-policy" aria-label={t('privacy_policy.heading')}>
       <Card>
         <StyledHeading>{t('privacy_policy.heading')}</StyledHeading>
-        <StyledNormalTextPreWrapped>{t('privacy_policy.description')}</StyledNormalTextPreWrapped>
+        <List>
+          <ListItem>
+            <Link
+              href="https://www.unit.no/sites/default/files/media/filer/2021/06/Personvernerkl%C3%A6ring%20-%20DLR.pdf"
+              target="_blank">
+              {t('privacy_policy.heading')} - {t('localization.norwegian_bokmaal')}
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link
+              href="https://www.unit.no/sites/default/files/media/filer/2021/06/Personvernerkl%C3%A6ring%20-%20DLR_en.pdf"
+              target="_blank">
+              {t('privacy_policy.heading')} - {t('localization.english')}
+            </Link>
+          </ListItem>
+        </List>
       </Card>
     </StyledInformationWrapper>
   );
