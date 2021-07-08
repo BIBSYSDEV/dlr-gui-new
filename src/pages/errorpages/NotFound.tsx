3959f5f865d4ac35dc520f8208e633fac3f1d615
anetteOlli <ansi@unit.no>
2021-07-08 11:31:43 +0200

    SMILE-649-creator-has-also-published, styling

--- src/pages/errorpages/NotFound.tsx
+++ src/pages/errorpages/NotFound.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
 import { PageHeader } from '../../components/PageHeader';
-import { StyledContentWrapperMedium } from '../../components/styled/Wrappers';
+import { StyledContentWrapperLarge, StyledContentWrapperMedium } from '../../components/styled/Wrappers';
 import ResourceInSpace from '../../resources/images/illustrations/resource_in_space.svg';
 
 const StyledImg = styled.img`
@@ -16,12 +16,12 @@ const NotFound = () => {
   const { t } = useTranslation();
 
   return (
-    <>
+    <StyledContentWrapperLarge>
       <PageHeader data-testid="404">{t('error.404_page')}</PageHeader>
       <StyledContentWrapperMedium>
         <StyledImg src={ResourceInSpace} alt={t('illustration_alts_tags.resource_in_space')} />
       </StyledContentWrapperMedium>
-    </>
+    </StyledContentWrapperLarge>
   );
 };
 
