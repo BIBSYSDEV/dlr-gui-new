ab85b7f5a40fb67e588c23252844146279f59f30
anetteOlli <44363838+anetteOlli@users.noreply.github.com>
2021-07-09 09:12:21 +0200

    SMILE-896-handle-not-found-route, functionality implemented (#611)
    * SMILE-896-handle-not-found-route, functionality implemented
    
    * SMILE-649-creator-has-also-published, code-review
    
    * SMILE-649-creator-has-also-published, styling
    
    * SMILE-896-handle-not-found-route, testing code-build without plugin
    
    * SMILE-896-handle-not-found-route, adding plugin file
    
    * SMILE-896-handle-not-found-route, code-review
    
    * SMILE-896-handle-not-found-route, code-review

---
+++ src/pages/errorpages/HandleNotFound.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+import ResourceInSpace from '../../resources/images/illustrations/resource_in_space.svg';
+import { StyledContentWrapperLarge, StyledContentWrapperMedium } from '../../components/styled/Wrappers';
+import { PageHeader } from '../../components/PageHeader';
+import { Typography } from '@material-ui/core';
+
+const StyledImg = styled.img`
+  @media (min-width: ${({ theme }) => theme.breakpoints.values.sm + 'px'}) {
+    width: 23rem;
+  }
+  width: 100%;
+`;
+
+const StyledTypography = styled(Typography)`
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+`;
+
+const HandleNotFound = () => {
+  const { t } = useTranslation();
+  return (
+    <StyledContentWrapperLarge>
+      <PageHeader testId="404">{`404 ${t('handle_not_found.handle_not_found')}`}</PageHeader>
+      <StyledContentWrapperMedium>
+        <StyledImg src={ResourceInSpace} alt={t('illustration_alts_tags.resource_in_space')} />
+        <StyledTypography>{t('handle_not_found.this_resource_has_been_deleted')}</StyledTypography>
+      </StyledContentWrapperMedium>
+    </StyledContentWrapperLarge>
+  );
+};
+
+export default HandleNotFound;
