0a48000038d51bc3aed010135cfd97cb8cba8636
anetteOlli <ansi@unit.no>
2021-08-09 15:18:12 +0200

    SMILE-535-polishing-distance-between-footer-header-and-content, code-review

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -3,8 +3,11 @@ import styled from 'styled-components';
 import { Typography } from '@material-ui/core';
 
 const StyledHeader = styled(Typography)`
-  border-bottom: 3px solid;
   margin-top: 4.6rem;
+  @media (max-width: ${({ theme }) => theme.breakpoints.values.md + 'px'}) {
+    margin-top: 2rem;
+  }
+  border-bottom: 3px solid;
   margin-bottom: 2rem;
   padding-bottom: 0.5rem;
 `;
