19ac047eb29cc89167877c519fbad4a0b1fc9556
anetteOlli <44363838+anetteOlli@users.noreply.github.com>
2021-08-10 12:03:17 +0200

    Smile 688 do not display cc on licence (#660)
    * SMILE-688-do-not-display-cc-on-licence, removed version from displaying
    
    * SMILE-688-do-not-display-cc-on-licence, bug fix in licenseWizard showing recommended license text before user has selected anything in form
    
    * SMILE-688-do-not-display-cc-on-licence, code-review
    
    * SMILE-688-do-not-display-cc-on-licence, code-review

--- src/pages/edit_resource/access_and_licence_step/LicenseWizardFields.tsx
+++ src/pages/edit_resource/access_and_licence_step/LicenseWizardFields.tsx
@@ -420,7 +420,7 @@ const LicenseWizardFields: FC<LicenseWizardFieldsProps> = ({
             )}
           </AccordionRadioGroup>
         )}
-        {values.licenses[0].features?.dlr_license_code && (
+        {values.licenses[0].features?.dlr_license_code && values.features.dlr_licensehelper_resource_restriction && (
           <>
             {allChangesSaved && recommendedLicense === values.licenses[0].features.dlr_license_code && (
               <StyledTypography data-testid="recommended-license">
@@ -429,7 +429,10 @@ const LicenseWizardFields: FC<LicenseWizardFieldsProps> = ({
             )}
             {allChangesSaved && recommendedLicense !== values.licenses[0].features.dlr_license_code && (
               <StyledHeavyWeightTypography data-testid="recommended-license">
-                {t('license.recommended_license_is', { license: recommendedLicense })}.
+                {t('license.recommended_license_is', {
+                  license: recommendedLicense.replace(' 4.0', ''),
+                })}
+                .
               </StyledHeavyWeightTypography>
             )}
             {!allChangesSaved && <CircularProgress size="2rem" />}
