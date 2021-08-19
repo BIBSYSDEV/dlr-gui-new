dfc9adbe6cc29ba0569592dd76ef7a4a8b372880
anetteOlli <ansi@unit.no>
2021-08-19 12:27:38 +0200

    SMILE-962-missing-embed-src-url, code-review

--- src/components/ContentPreview.tsx
+++ src/components/ContentPreview.tsx
@@ -167,9 +167,11 @@ const ContentPreview: FC<ContentPreviewProps> = ({ resource, isPreview = false, 
             <>
               <StyledLoginInformationWrapper>
                 {!user.id && (
-                  <Typography gutterBottom>{t('resource.preview.no_preview_authorization_reasons')}</Typography>
+                  <>
+                    <Typography gutterBottom>{t('resource.preview.no_preview_authorization_reasons')}</Typography>
+                    <LoginButton variant="contained" />
+                  </>
                 )}
-                {!user.id && <LoginButton variant="contained" />}
                 {user.id && <Typography>{t('resource.preview.your_user_does_not_have_access')}</Typography>}
               </StyledLoginInformationWrapper>
             </>
